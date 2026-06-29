d916591065827c83c2fe40a89f7011db0427b405
Patience Jelagat <patiencejelagatt@gmail.com>
2026-06-29 14:49:56 +0300

    Complete home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import Header from "./components/header/Header"
-import Hero from "./components/hero/Hero"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Gallery from "./pages/Gallery";
+// import Services from "./pages/Services";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="app-container">
-         <Header />
-         <Hero />
-    </div>
+    <BrowserRouter>
+
+      <div className="app-container">
+
+          <Header />
+
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} /> */}
+
+          </Routes>
+
+      </div>
+
+    </BrowserRouter>  
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,120 @@
+import Hero from "../components/hero/Hero";
+import "./Home.css"
+import homeAboutImage from "../assets/images/adrian-fernandez-t1jlLrsMpjc-unsplash.jpg"
+
+function HomeAbout(){
+    return(
+        <div className="home-about">
+
+            <div className="stacked-divs">
+
+                <div className="first-div">
+                    <img src={homeAboutImage} alt="" />
+                </div>
+                <div className="second-div"></div>
+                <div className="third-div"></div>
+                <div className="fourth-div"></div>
+
+            </div>
+
+            <div className="home-about-text">
+                <h3>What We Do:</h3>
+                <p>At Pala Lume, we believe that healthy hair is the foundation of every great style. Specializing in textured and natural hair care, we are dedicated to providing personalized services that celebrate the beauty and versatility of every client.</p>
+                <p>Whether you're looking for protective styling, restorative treatments, or a fresh new look, our approach combines professional expertise with genuine care to help you achieve your hair goals.</p>
+                <p>We strive to create a welcoming and relaxing environment where every appointment feels like a self-care experience. By focusing on healthy hair practices, quality products, and attention to detail, we help our clients maintain strong, beautiful hair while expressing their unique style with confidence.</p>
+
+                 <div className="home-about-cta">
+                    <button className="generic">Learn More</button>
+                    <button className="primary">Book Appointment</button>
+                 </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+function Carousel (){
+
+    return(
+
+        <div className="home-about-carousel">
+
+            <div className="carousel-cards">
+
+                <article>
+                    <img src={homeAboutImage}  alt="" />
+                </article>
+
+                <article>
+                    <div className="carousel-card-text">
+                        <h4>500+</h4>
+                        <p>Healthy Hair Transformations</p>
+                    </div>
+                </article>
+                            
+                <article>
+                    <img src={homeAboutImage} alt="" />
+                </article>
+
+                <article>
+                    <div className="carousel-card-text">
+                        <h4>100%</h4>
+                        <p>Client-Focused Care</p>
+                    </div>
+                </article>
+
+                <article>
+                    <img src={homeAboutImage}  alt="" />
+                </article>
+
+                <article>
+                    <div className="carousel-card-text">
+                        <h4>500+</h4>
+                        <p>Healthy Hair Transformations</p>
+                    </div>
+                </article>
+                            
+                <article>
+                    <img src={homeAboutImage} alt="" />
+                </article>
+
+                <article>
+                    <div className="carousel-card-text">
+                        <h4>100%</h4>
+                        <p>Client-Focused Care</p>
+                    </div>
+                </article>
+
+                <article>
+                    <img src={homeAboutImage}  alt="" />
+                </article>
+
+                <article>
+                    <div className="carousel-card-text">
+                        <h4>1000+</h4>
+                        <p>Satisfied Return Clients</p>
+                    </div>
+                </article>
+
+            </div>
+            
+        </div>
+
+    );
+     
+}
+
+function Home (){
+    return(
+        <div className="home-container">
+
+         <Hero />
+         <HomeAbout />
+         <Carousel />
+          
+        </div>
+    );
+}
+
+export default Home;
