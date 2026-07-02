39f0355633cf4e8d9a31697b7ac52abf5224bd1b
Patience Jelagat <patiencejelagatt@gmail.com>
2026-07-02 16:57:09 +0300

    pre-images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,96 @@
 import Hero from "../components/hero/Hero";
 import "./Gallery.css"
-import galleryHero from "../assets/images/guilherme-petri-PtOfbGkU3uI-unsplash.jpg"
 import searchIcon from "../assets/icons/magnifying-glass.png"
-import productImage from "../assets/images/minitwist 1.png"
+import galleryHero from "../assets/images/guilherme-petri-PtOfbGkU3uI-unsplash.jpg"
+
+import { useState } from "react";
+
+const images = import.meta.glob("../assets/images/*.{png}", {
+    eager: true,
+    import: "default"
+});
 
 function GalleryContent(){
+
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
+        const hairstyles = [
+            {
+                id: 1,
+                image: images["../assets/images/twist1.jpg"],
+                category: "Twists"
+            },
+            {
+                id: 2,
+                image: "/images/twists2.jpg",
+                category: "Twists"
+            },
+            {
+                id: 3,
+                image:"/images/twists3.jpg",
+                category: "Twists"
+            },
+            {
+                id: 4,
+                image: "/images/twists4.jpg",
+                category: "Twists"
+            },
+            {
+                id: 5,
+                image: "/images/braids1.jpg",
+                category: "Braids"
+            },
+            {
+                id: 6,
+                image: "/images/braids2.jpg",
+                category: "Braids"
+            },
+            {
+                id: 7,
+                image: "/images/braids3.jpg",
+                category: "Braids"
+            },
+            {
+                id: 8,
+                image: "/images/braids4.jpg",
+                category: "Braids"
+            },
+            {
+                id: 9,
+                image: "/images/locs1.jpg",
+                category: "Locks"
+            },
+            {
+                id: 10,
+                image:"/images/locs2.jpg",
+                category: "Locks"
+            },
+            {
+                id: 11,
+                image: "/images/locs3.jpg",
+                category: "Locks"
+            },
+            {
+                id: 12,
+                image: "/images/blowout1.jpg",
+                category: "Blowout"
+            },
+            {
+                id: 13,
+                image: "/images/blowout2.jpg",
+                category: "Blowout"
+            },
+            {
+                id: 14,
+                image: "/images/blowout3.jpg",
+                category: "Blowout"
+            }
+        ];
+
+    const filteredImages = 
+     selectedCategory === "All" ? hairstyles : hairstyles.filter(
+        style => style.category === selectedCategory
+     );
 
     const styles = [
         "Twists",
@@ -13,6 +99,7 @@ function GalleryContent(){
         "Blowout",
         "Curl Definition"      
     ];
+
 
     return(
         <div className="gallery-content">
@@ -36,7 +123,7 @@ function GalleryContent(){
                         {[...styles, ...styles, ...styles].map((style, index) => (
                             <button
                                 key={index}
-                                onClick={() => console.log(style)}
+                                onClick={() => setSelectedCategory("")}
                             >
                                 {style}
                             </button>
@@ -48,7 +135,13 @@ function GalleryContent(){
             </div>
 
             <div className="catalogue">
-
+                {filteredImages.map(style => (
+                    <img
+                        key={style.id}
+                        src={style.image}
+                        alt={style.category}
+                    />
+                ))}
             </div>
 
         </div>
@@ -58,6 +151,7 @@ function GalleryContent(){
 function Gallery(){
     return(
         <div className="gallery-container">
+        { console.log(images)}
              <Hero 
                buttonDisplay={{display: "none"}}
                heading={"Our Gallery"}
